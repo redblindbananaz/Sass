84b36f2a3d68e5fb9cb4cb557028e8eca4e10a8a
redblindbananaz <sebhilltop@gmail.com>
2024-09-06 16:58:27 +1200

    refactor and navigation setup

---
+++ js/hamburger.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav_item');
+
+let showMenu = false;
+menu.addEventListener('click', toggle);
+function toggle() {
+    if (!showMenu) {
+        menu.classList.add('open');
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        showMenu = true;
+    } else {
+        menu.classList.remove('open');
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        showMenu = false;
+    }
+}
